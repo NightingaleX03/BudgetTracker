02da1117ee864a33c6eeade910865251b4a36d1f
NightingaleX03 <132782701+NightingaleX03@users.noreply.github.com>
2025-02-04 21:56:06 -0500

    Update expense.py

--- expense.py
+++ expense.py
@@ -1,11 +1,15 @@
-import json
+from typing_extensions import Self
+
+
+from typing import Any
+
+
 from datetime import datetime
 
 class Expense:
 
     # list to store all expenses
     expenses = []
-    budget = 0
 
     # constructor
     def __init__(self, description, amount, category="other", date=None):
@@ -17,7 +21,7 @@ class Expense:
         self.date = date if date else datetime.now().strftime("%Y-%m-%d")
 
     @property
-    def dict(self):
+    def dict(self) -> dict[str, Any | str]:
         # Convert the expense object to a dictionary
         return {
             "description": self.description,
@@ -30,7 +34,7 @@ class Expense:
     @staticmethod
     def add_expense():
 
-        # get description, amount, date from user
+         # get description, amount, date from user
         description = input("Enter description: ")
         amount = input("Enter amount: ")
 
@@ -39,11 +43,12 @@ class Expense:
             amount = input("Invalid amount. Please enter a valid number: ")
 
         date = input("Enter date (YYYY-MM-DD) or leave blank for today's date: ")
+        
         # date not selected use current date
         if not date:
             date = datetime.now().strftime("%Y-%m-%d")
 
-        # get category from user
+         # get category from user
         print("Please select a category:\n\t(f) Food\n\t(t) Transport\n\t(u) Utilities\n\t(r) Rent\n\t(o) Other\n")
         category = input("Enter choice: ").lower()
         category_mapping = {'f': 'food', 't': 'transport', 'u': 'utilities', 'r': 'rent', 'o': 'other'}
@@ -51,33 +56,32 @@ class Expense:
 
         # create an instance of Expense
         expense = Expense(description, amount, category, date)
-
+        
         # append the expense to expenses list
         Expense.expenses.append(expense)
 
     # method to view expenses
     @staticmethod
-    def view_expenses():
-
+    def view_expenses(budget_checker):
         if not Expense.expenses:
             print("No expenses available")
         else:
             for expense in Expense.expenses:
                 print(f"Description: {expense.description}, Amount: {expense.amount}, Category: {expense.category}, Date: {expense.date}")
 
-        Expense.check_budget()
+        budget_checker(Expense.expenses)
 
     # method to filter expenses
     @staticmethod
-    def filter_expense(filter_choice):
-        Expense.load_expense_file()
+    def filter_expense(filter_choice, file_loader):
+        file_loader()
 
-        # filter by keyword
+        #  filter by keyword
         if filter_choice == 'k':
             keyword = input("Enter keyword: ")
             filtered_expenses = [expense for expense in Expense.expenses if keyword.lower() in expense.description.lower()]
 
-        # filter by date range
+         # filter by date range
         elif filter_choice == 'd':
             start_date = input("Enter start date (YYYY-MM-DD): ")
             end_date = input("Enter end date (YYYY-MM-DD): ")
@@ -99,77 +103,3 @@ class Expense:
         else:
             for expense in filtered_expenses:
                 print(f"Description: {expense.description}, Amount: {expense.amount}, Category: {expense.category}, Date: {expense.date}")
-
-    # method to save expenses to file
-    @staticmethod
-    def save_expense_file():
-        try:
-            with open("expenseList.json", "r") as file:
-                exist_expenses = json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
-            exist_expenses = []
-
-        new_expenses = [expense.dict for expense in Expense.expenses if expense.dict not in exist_expenses]
-        if new_expenses:
-            with open("expenseList.json", "w") as file:
-                json.dump(exist_expenses + new_expenses, file)
-                print("Expenses saved successfully.")
-        else:
-            print("No new expenses to save.")
-
-    # method to load expenses from file
-    @staticmethod
-    def load_expense_file():
-
-        # load expenses from file
-        try:
-            with open("expenseList.json", "r") as file:
-
-                anything = file.read().strip()
-                if not anything:
-                    print("No expenses available")
-                    return
-
-                # convert json string to list of dictionaries
-                Expense.expenses = [Expense(**expense) for expense in json.loads(anything)]
-
-        # file not found
-        except FileNotFoundError:
-            print("No expense file found.")
-
-    # method to set budget
-    @staticmethod
-    def set_budget():
-
-        # get budget from user
-        budget_temp = input("Enter budget: ")
-
-        while not budget_temp.isdigit():
-            budget_temp = input("Invalid input. Please enter a numeric budget: ")
-
-        Expense.budget = int(budget_temp)
-        print(f"Budget set to {Expense.budget}")
-
-    # method to check budget
-    @staticmethod
-    def check_budget():
-        # calculate total expenses
-        total = sum([int(expense.amount) for expense in Expense.expenses])
-        print(f"Total expenses so far: {total}")
-
-        # check if total expenses go over budget
-        if total > Expense.budget:
-            print("Please stop spending money! You have exceeded your budget!")
-        
-        # check if total expenses is below budget
-        else:
-            print("You are within your budget! Spend away!")
-
-    # method to check total expenses from file
-    @staticmethod
-    def check_total_expenses():
-
-        # load expenses from file
-        Expense.load_expense_file()
-        total_expenses = sum([int(expense.amount) for expense in Expense.expenses])
-        print(f"Total expenses from file: {total_expenses}")
